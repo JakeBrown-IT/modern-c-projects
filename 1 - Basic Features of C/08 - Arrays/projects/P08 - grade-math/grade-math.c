aa8005916b0592b2ae3c8be2447f20fd311476b4
JakeBrown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-05-23 23:58:49 +0100

    Completed the project - Wildly ugly solution, but it works lmao

--- 1 - Basic Features of C/08 - Arrays/projects/P08 - grade-math/grade-math.c
+++ 1 - Basic Features of C/08 - Arrays/projects/P08 - grade-math/grade-math.c
@@ -1,6 +1,6 @@
 /* Programming Project 08 
  *
- * calculate average scorefor each student
+ * calculate average score for each student
  * 
  * Student No.  |  1  |  2  |  3  |  4  |  5 
  * Avr. Score   |  -  |  -  |  -  |  -  |  -
@@ -22,14 +22,103 @@
 int main(void) {
     int grades[STUDENTS][SCORES] = {0};
 
+    // assign all scores to students
     // iterate over rows
     for (int i = 0; i < STUDENTS; i++) {
-        printf("Enter row %d: ", i + 1);
+        printf("Enter scores for student %d: ", i + 1);
         // iterate over columns, to fill each slot up
         for (int j = 0; j < SCORES; j++) {
             scanf("%d", &grades[i][j]);
         }
     }
+
+    // now we print each student number for the table
+    printf("\nStudent No.  |  ");
+
+    for (int i = 0; i < STUDENTS; i++) {
+        printf("%5d  ", i + 1);
+    }
+
+    printf("\nTotal Score  |  ");
+
+    int total;
+
+    for (int i = 0; i < STUDENTS; i++) {
+        total = 0;
+
+        for (int j = 0; j < SCORES; j++) {
+            total += grades[i][j];
+        }
+
+        printf("%5d  ", total);
+    }
+
+    printf("\nAvr. Score   |  ");
+
+    double average;
+
+    for (int i = 0; i < STUDENTS; i++) {
+        average = 0;
+
+        for (int j = 0; j < SCORES; j++) {
+            average += grades[i][j];
+        }
+
+        average /= SCORES;
+
+        printf("%5.3f  ", average);
+    }
+
+    printf("\n");
+
+    // now for working out the quiz values
+    printf("\nQuiz No.     |  ");
+
+    int lowest, highest;
+
+    for (int i = 0; i < STUDENTS; i++) {
+        printf("%5d  ", i + 1);
+    }
+
+    printf("\nAvr. Score   |  ");
+
+    for (int i = 0; i < STUDENTS; i++) {
+        average = 0;
+
+        for (int j = 0; j < SCORES; j++) {
+            average += grades[j][i];
+        }
+
+        average /= SCORES;
+
+        printf("%5.3f  ", average);
+    }
+
+    printf("\nHighest      |  ");
+
+    for (int i = 0; i < STUDENTS; i++) {
+        highest = grades[0][0];
+        for (int j = 0; j < SCORES; j++) {
+            if (grades[j][i] > highest) {
+                highest = grades[j][i];
+            }
+        }
+        printf("%5d  ", highest);
+    }
+
+    printf("\nLowest       |  ");
+
+    for (int i = 0; i < STUDENTS; i++) {
+        lowest = grades[0][0];
+        for (int j = 0; j < SCORES; j++) {
+            if (grades[j][i] < lowest) {
+                lowest = grades[j][i];
+            }
+        }
+        printf("%5d  ", lowest);
+    }
+
+    printf("\n");
 
     return 0;
 }
