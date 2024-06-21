75baecd1ac1bded8590487cb3a1dba3473a61734
JakeBrown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-06-21 19:05:53 +0100

    finished exercise size 10

--- 1 - Basic Features of C/09 - Functions/exercises/E10 - array-functions/array-functions.c
+++ 1 - Basic Features of C/09 - Functions/exercises/E10 - array-functions/array-functions.c
@@ -2,22 +2,27 @@
 
 #include <stdio.h>
 
+int largest(int n, int arr[]);
+int average(int n, int arr[]);
+int positive(int n, int arr[]);
+
 int main(void)
 {
-    int n;
+    // initialise array with max length of 100
+    int arr[100] = {0};
+    int n, i = 0;
 
+    // get length of array
     printf("Enter the size of the array: ");
     scanf("%d", &n);
 
-    int arr[n];
-
-    printf("Enter %d integers: ");
-
-    for (int i = 0; i < n; i++)
-    {
-        scanf("%d", arr[i]);
+    // set the indexes in array to values
+    printf("Enter %d integers: ", n);
+    for (; i < n; i++) {
+        scanf("%d", &arr[i]);
     }
 
+    // print outputs of functions
     printf("Largest element in array: %d\n", largest(n, arr));
     printf("Average of elements in array: %d\n", average(n, arr));
     printf("Number of positive elements in array: %d\n", positive(n, arr));
@@ -25,4 +30,36 @@ int main(void)
     return 0;
 }
 
-int largest(int size, int arr[]);
+// get largest element in array
+int largest(int n, int arr[]) {
+    int i = 0, max = arr[0];
+
+    for (; i < n; i++) {
+        if (arr[i] > max) {
+            max = arr[i];
+        }
+    } 
+    return max;
+}
+
+// get average of all elements in array
+int average(int n, int arr[]) {
+    int i = 0, avr = 0;
+
+    for (; i < n; i++) {
+        avr += arr[i];
+    }
+    return avr / n; 
+}
+
+// get the count of positive elements in array
+int positive(int n, int arr[]) {
+    int i = 0, count = 0;
+
+    for (; i < n; i++) {
+        if (arr[i] > 0) {
+            count++;
+        }
+    }
+    return count;
+}
