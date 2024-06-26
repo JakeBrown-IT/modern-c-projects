cbe16c7ef302e18500a42f4f13a7a6c02b8ecefb
JakeBrown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-06-26 16:04:28 +0100

    Finished Functions Exercise 18 - gcd recursive

--- 1 - Basic Features of C/09 - Functions/exercises/E18 - gcd-recursive/gcd-recursive.c
+++ 1 - Basic Features of C/09 - Functions/exercises/E18 - gcd-recursive/gcd-recursive.c
@@ -1,5 +1,17 @@
 /* Functions Exercise - gcd recursive */
 
-int gcd(int n, int r);
+#include <stdio.h>
 
-int main()
+int gcd(int n, int m);
+
+int main(void)
+{
+    printf("GCD of 10 and 2: %d\n", gcd(10, 2));
+
+    return 0;
+}
+
+int gcd(int n, int m)
+{
+    return n == 0 ? m : gcd(m % n, m);
+}
