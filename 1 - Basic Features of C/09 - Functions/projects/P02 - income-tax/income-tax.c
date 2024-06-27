1f88a80098748fd9c904fcddb2370787df4bf036
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-06-27 20:06:44 +0100

    Finished Functions Project 02 - income-tax

--- 1 - Basic Features of C/09 - Functions/projects/P02 - income-tax/income-tax.c
+++ 1 - Basic Features of C/09 - Functions/projects/P02 - income-tax/income-tax.c
@@ -1,1 +1,33 @@
 /* Arrays Project 02 - income-tax.c */
+
+#include <stdio.h>
+
+float calculate_tax(float income);
+
+int main(void) {
+    double tax, income;
+
+    printf("Enter amount of taxable income: %c", (char) 156);
+    scanf("%lf", &income);
+
+    tax = calculate_tax(income);
+
+    printf("Tax Due: %c%.2lf\n", (char) 156, tax);
+
+    return 0;
+}
+
+float calculate_tax(float income) {
+    if (income <= 750.0f)
+        return income * 0.01f;
+    else if (income <= 2250.0f)
+        return 7.5f + (income - 750.0f) * 0.02f;
+    else if (income <= 3750.0f)
+        return 37.5f + (income - 2250.0f) * 0.03f;
+    else if (income <= 5250.0f)
+        return 82.5f + (income - 3750.0f) * 0.04f;
+    else if (income <= 7000.0f)
+        return 142.5f + (income - 5250.0f) * 0.05f;
+    else
+        return 230.0f + (income - 7000.0f) * 0.06f;
+}
