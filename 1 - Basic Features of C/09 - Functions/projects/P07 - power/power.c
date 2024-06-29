33787b133ed2759b9d51f238fb891c783dc9e994
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-06-29 17:59:45 +0100

    Finished Functions Project 07 - power

--- 1 - Basic Features of C/09 - Functions/projects/P07 - power/power.c
+++ 1 - Basic Features of C/09 - Functions/projects/P07 - power/power.c
@@ -11,16 +11,22 @@ int main(void)
     printf("Enter a value for x and n: ");
     scanf("%d %d", &x, &n);
 
-    printf("x^n = %d\n", power(x, n));
+    printf("%d^%d = %d\n", x, n, power(x, n));
 
     return 0;
 }
 
 int power(int x, int n)
 {
+    int result = 1;
+
     if (n == 0)
         return 1;
     else
-        if (n % 2 == 0)
-            x = 
+        if (n % 2 == 0) {
+            result = power(x, n / 2);
+            return result * result;
+        }
+        else 
+            return x * power(x, n - 1);
 }
