43d17654804c484fc28e879ecff37ec96c2185bd
Jake <56654395+JakeBrown-IT@users.noreply.github.com>
2024-06-29 11:36:34 +0100

    Finished Functions Project 06 - polynomial

--- 1 - Basic Features of C/09 - Functions/projects/P06 - polynomial/polynomial.c
+++ 1 - Basic Features of C/09 - Functions/projects/P06 - polynomial/polynomial.c
@@ -1,1 +1,25 @@
 /** Functions Project 96 - polynomial.c */
+
+#include <stdio.h>
+#include <math.h>
+
+int polynomial(int x);
+
+int main(void)
+{
+    int x, result;
+
+    printf("Enter a value for x: ");
+    scanf("%d", &x);
+
+    result = polynomial(x);
+
+    printf("3x^5 + 2x^4 - 5x^3 - x^2 + 7x - 6  ...  where x = %d,\n = %d\n", x, result);
+
+    return 0;
+}
+
+int polynomial(int x)
+{
+    return (3 * pow(x, 5)) + (2 * pow(x, 4)) - (5 * pow(x, 3)) - pow(x, 2) + (7 * x) - 6;
+}
