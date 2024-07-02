2ddb00b77f46fb6808ab29aa944e7a127b4c5f1e
JakeBrown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-07-02 19:00:42 +0100

    Finished Section 10 Project 01 - stack

---
+++ 1 - Basic Features of C/10 - Program Organization/projects/P01 - stack/README.md
@@ -0,0 +1,10 @@
+# Program Organization Project 01 - stack
+
+Modify the stack example of Section 10.2 so that it stores characters isntead of integers. Next, add a `main` function that asks the user to enter a series of parentheses and/or braces, then indicates whether or not they're properly nested:
+
+```text
+Enter parentheses and/or braces: ((){}{()})
+Parentheses/braces are nested properly
+```
+
+Hint: As the program reads the characters, have it push each left parenthesis or left brace. When it reads a right parenthesis or brace, have it pop the stack and check that the item popped is a matching parenthesis or brace. (If not, the parentheses/braces aren't nested properly.) When the program reads the new-line character, have it check whether the stack is empty; if so, the parentheses/braces are matched. If the stack isn't empty (or if `stack_underflow` is ever called), the parentheses/braces aren't matched. If `stack_overflow` is called, have the program print the message `Stack overflow` and terminate immediately.
---
+++ 1 - Basic Features of C/10 - Program Organization/projects/P01 - stack/stack.c
@@ -0,0 +1,112 @@
+/** Program Organization Project 01 - stack.c */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> // C99 Only
+
+#define STACK_SIZE 100
+
+// External variables
+char stack[STACK_SIZE];
+int top = 0;
+
+void stack_overflow();
+void stack_underflow();
+void make_empty(void);
+bool is_empty(void);
+bool is_full(void);
+void push(char ch);
+int pop(void);
+
+int main(void)
+{
+    char ch, check;
+
+    printf("Enter parentheses and/or braces: ");
+
+    while ((ch = getchar()) != '\n')
+    {
+        if (ch == '(' || ch == '{')
+        {
+            push(ch);
+        }
+        else if (ch == ')')
+        {
+            check = pop();
+
+            if (check == '(')
+            {
+                continue;
+            }
+            else
+            {
+                printf("Parentheses/braces aren't nested properly\n");
+                return 0;
+            }
+        }
+        else if (ch == '}')
+        {
+            check = pop();
+
+            if (check == '{')
+            {
+                continue;
+            }
+            else
+            {
+                printf("Parentheses/braces aren't nested properly\n");
+                return 0;
+            }
+        }
+        else
+        {
+            continue;
+        }
+    }
+
+    printf("Parentheses/braces are nested properly\n");
+
+    return 0;
+}
+
+void stack_overflow(void)
+{
+    printf("Stack overflow\n");
+}
+
+void stack_underflow(void)
+{
+    printf("Stack underflow\n");
+}
+
+void make_empty(void)
+{
+    top = 0;
+}
+
+bool is_empty(void)
+{
+    return top == 0;
+}
+
+bool is_full(void)
+{
+    return top == STACK_SIZE;
+}
+
+void push(char ch)
+{
+    if (is_full())
+        stack_overflow();
+    else
+        stack[top++] = ch;
+}
+
+int pop(void)
+{
+    if (is_empty()) {
+        stack_underflow();
+        exit(0);
+    }
+    else return stack[--top];
+}
