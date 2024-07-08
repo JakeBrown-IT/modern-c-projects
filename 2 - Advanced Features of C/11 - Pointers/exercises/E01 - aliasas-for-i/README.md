533203d149b57f5df7cd1c70875a117e37ac7020
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-07-08 22:39:20 +0100

    Finished Pointers Exercise 01 - aliases. Also started Pointers Exercise 02 - legal-assignment

---
+++ 2 - Advanced Features of C/11 - Pointers/exercises/E01 - aliasas-for-i/README.md
@@ -0,0 +1,10 @@
+# Pointers Exercise 01 - aliases for i
+
+If `i` is a variable and `p` points to `i`, which of the following expressions are aliases for `i`?
+
+(a) `*p`      (c) `*&p`    (e) `*i`      (g) `*&i`
+(b) `&p`      (d) `&*p`    (f) `&i`      (h) `&*i`
+
+Solution
+
+Aliases for `i`: a, g
---
+++ 2 - Advanced Features of C/11 - Pointers/exercises/E01 - aliasas-for-i/aliases.c
@@ -0,0 +1,28 @@
+/** Pointers Exercise 01 - aliases.c 
+ * 
+ * Example of an alias:
+ * -- int i = 1, *p = &i;
+ * 
+ * *p is an alias of i
+ * -- if i = 1, *p = 1
+ * -- if *p = 2, i = 2 
+*/
+
+
+#include <stdio.h>
+
+int main(void) {
+    int i = 1, *p;
+    p = &i;
+
+    printf("%d\n", *p);     // alias
+    //printf("%d\n", &p);   // not alias 
+    //printf("%d\n", *&p);  // not alias
+    //printf("%d\n", &*p);  // not alias
+    //printf("%d\n", *i);   // not alias
+    //printf("%d\n", &i);   // not alias
+    printf("%d\n", *&i);    // alias
+    //printf("%d\n", &*i);  // not alias
+
+    return 0;
+}
