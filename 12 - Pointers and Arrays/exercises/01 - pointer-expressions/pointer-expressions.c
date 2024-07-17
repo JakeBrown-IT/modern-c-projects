23f4436b550eb1f5d713125d92d5b4749e4993f8
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-07-17 10:48:44 +0100

    Added Section 12 Exercise 01 -- pointer-expressions

---
+++ 12 - Pointers and Arrays/exercises/01 - pointer-expressions/pointer-expressions.c
@@ -0,0 +1,26 @@
+/** Section 12 Exercise 01
+ * Author -- Jake Brown
+ * Title  -- pointer-expressions.c
+ */
+
+#include <stdio.h>
+
+int main(void) {
+	int a[] = {5, 15, 34, 54, 14, 2, 52, 72};
+	int *p = &a[1], *q = &a[5];
+
+	/* (a) *(p + 3) ==> 14
+	 * (b) *(q - 3) ==> 34
+	 * (c) q - p    ==> 4	  
+	 * (d) p < q    ==> 0
+	 * (e) *p < *q  ==> 1
+	 */
+
+	printf("*(p + 3) ==> %d\n", *(p + 3));
+	printf("*(q - 3) ==> %d\n", *(q - 3));
+	printf("   q - p ==> %d\n", q - p);
+	printf("   p < q ==> %d\n", p < q);
+	printf(" *p < *q ==> %d\n", *p < *q);
+
+	return 0;
+}
