622aec7c12a70e783e810fd27f80c0764468a3fe
JakeBrown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-08-31 18:07:41 +0100

    Finished Section 12 Project 04 -- palindrome-v2

--- 12 - Pointers and Arrays/projects/04 - palindrome-v2/palindrome-v2.c
+++ 12 - Pointers and Arrays/projects/04 - palindrome-v2/palindrome-v2.c
@@ -5,11 +5,8 @@
 
 #include <ctype.h>
 #include <stdio.h>
-#include <stdbool.h>
 
 #define MAX_LEN 20
-
-bool is_palindrome(char *pI, char *pJ);
 
 int main(void) {
 
@@ -19,25 +16,18 @@ int main(void) {
     
     printf("Enter a message: ");
     
-    while ((ch = getchar()) != '\n' && pI < msg + MAX_LEN) {
+    while ((ch = tolower(getchar())) != '\n' && pI < msg + MAX_LEN)
         if (isalpha(ch))
-            *pJ++ = tolower(ch);
-    }
-    pJ--;
+            *pI++ = ch;
+    pI--;
 
-    if (is_palindrome(pI, pJ))
-        printf("Palindrome\n");
-    else
-        printf("Not a palindrome\n");
+    for (pJ = msg; pJ < pI; pJ++, pI--)
+        if (*pI != *pJ) {
+            printf("Not a palindrome\n");
+            return 0;
+        }
+
+    printf("Palindrome\n");
 
     return 0;
 }
-
-bool is_palindrome(char *pI, char *pJ) {
-    while (pI < pJ)
-        if (*pI++ != *pJ--)
-            return false;
-
-    return true;
-}
-
