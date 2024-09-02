c798daf2a2c9d9dc77de3b5de76923428ce3951b
JakeBrown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-09-02 20:26:28 +0100

    Finished Section 12 Project 05 -- reverse-sentence

--- 12 - Pointers and Arrays/projects/05 - reverse-sentence/reverse-sentence.c
+++ 12 - Pointers and Arrays/projects/05 - reverse-sentence/reverse-sentence.c
@@ -5,7 +5,46 @@
 
 #include <stdio.h>
 
+#define MAX_SIZE 100
+
 int main(void)
 {
+    char ch, term, sentence[MAX_SIZE] = {0}, *p, *q;
+
+    printf("Enter a sentence: ");
+
+    for (p = sentence; (ch = getchar()) != '\n' && p < sentence + MAX_SIZE; p++) {
+        if (ch == '.' || ch == '?' || ch == '!') {
+            term = ch;
+            break;
+        }
+        *p = ch;
+    }
+
+    printf("Reversal of sentence: ");
+    // while p is not at beginning of the sentence
+    while (p >= sentence) {
+        // we need to find the beginning of the last word
+        // so we decrement p and break if it is a space or it's at index 0
+        while (*--p != ' ' && p != sentence)
+            ;   
+        // set q to the start of the next word
+        if (p == sentence)
+            q = sentence;
+        else
+            q = p + 1;
+
+        // while q is not at end of word and not end of string
+        while (*q != ' ' && *q != '\0')
+            putchar(*q++);  // print character and increment q        
+
+        // if p is not at index 0, print a space for the next word
+        if (p >= sentence)
+            putchar(' ');
+    }
+
+    // finally print the terminating character of the sentence
+    printf("\b%c\n", term);
+
     return 0;
 }
