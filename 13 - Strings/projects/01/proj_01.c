e8788ff09decd842fd31353495de811a94db6642
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-10-01 00:46:25 +0100

    fix: corrected section 13 project 01
    Program compiled and ran correctly from within the terminal in 'zed', but returned a segmentation fault from another terminal. This is now corrected and the program functions as expected.

--- 13 - Strings/projects/01/proj_01.c
+++ 13 - Strings/projects/01/proj_01.c
@@ -9,26 +9,49 @@
 
 #define MAXLEN 20
 
+/* globals */
+char smallest_word[MAXLEN + 1];
+char largest_word[MAXLEN + 1];
+char word[MAXLEN + 1];
+
+/* functions */
+void readWord(void);
+void readFirst(void);
+void readNext(void);
+
+/* main */
 int main(void) {
-    char smallest_word[MAXLEN], largest_word[MAXLEN];
-    char *word;
+    readFirst();
 
-    while (1) {
-        printf("Enter word: ");
-        scanf("%s", word);
-
-        if (strlen(word) < strlen(smallest_word))
-            strcpy(smallest_word, word);
-
-        if (strlen(word) > strlen(largest_word))
-            strcpy(largest_word, word);
-
-        if (strlen(word) == 4)
-            break;
-    }
+    while (strlen(word) != 4)
+        readWord();
 
     printf("Smallest word: %s\n", smallest_word);
     printf("Largest word: %s\n", largest_word);
 
     return 0;
+}
+
+/* get a word from the user */
+void readWord(void) {
+    printf("Enter word: ");
+    scanf("%20s", word);
+}
+
+/* get the first word from the user and initialise vars */
+void readFirst(void) {
+    readWord();
+
+    strcpy(smallest_word, word);
+    strcpy(largest_word, word);
+}
+
+/* get the next word from the user and compare */
+void readNext(void) {
+    readWord();
+
+    if (strcmp(word, smallest_word) < 0)
+        strcpy(smallest_word, word);
+    else if (strcmp(word, largest_word) > 0)
+        strcpy(largest_word, word);
 }
