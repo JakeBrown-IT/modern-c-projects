98ba57d44d234502a528af0bbdac7f86383a155c
Jake Brown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-10-09 23:08:54 +0100

    feat: added section 13 project 12

---
+++ 13 - Strings/projects/12/proj_12.c
@@ -0,0 +1,49 @@
+/* File: proj_12.c
+ * Author: Jake Brown
+ * Created: 09-10-2024
+ * Purpose: Section 13 Project 12
+ */
+
+#include <stdio.h>
+
+#define MAX_SIZE 100
+#define WORDLEN 20
+#define SENTLEN 30
+
+int main(void) {
+    char ch, term, sentence[SENTLEN][WORDLEN];
+    int i = 0, j = 0;
+
+    printf("Enter a sentence: ");
+ 
+    for (i = 0; i < SENTLEN; i++) {
+        for (j = 0; j < WORDLEN; j++) {
+            ch = getchar();
+
+            if (ch == ' ')
+                break;
+            else if (ch == '.' || ch == '!' || ch == '?') {
+                term = ch;
+                break;
+            }
+            else
+                sentence[i][j] = ch;
+        }
+
+        sentence[i][j] = '\0';
+
+        if (ch == ' ')
+            continue;
+        else
+            break; 
+    }        
+
+    printf("Reversal of sentence:");
+
+    for (; i >= 0; i--)
+        printf(" %s", sentence[i]);
+
+    printf("%c\n", term);
+
+    return 0;
+}
