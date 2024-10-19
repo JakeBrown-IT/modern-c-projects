bcaf59273acd173527bc002002c19e61df5202f3
JakeBrown <56654395+JakeBrown-IT@users.noreply.github.com>
2024-10-19 01:46:22 +0100

    fix: section 13 project 16 now uses both pointers
    feat: started section 13 project 17

---
+++ 13 - Strings/projects/17/proj_17.c
@@ -0,0 +1,40 @@
+/* File: proj_17.c
+ * Author: Jake Brown
+ * Created: 19-10-2024
+ * Purpose: Section 13 Project 17
+ */
+
+#include <ctype.h>
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX_LEN 20
+
+bool is_palindrome(const char *message);
+
+int main(void) {
+
+    char msg[MAX_LEN];
+    char ch;
+    char *pI = msg, *pJ = msg;
+    
+    printf("Enter a message: ");
+    
+    while ((ch = getchar()) != '\n' && pI < msg + MAX_LEN) {
+        if (isalpha(ch))
+            *pJ++ = tolower(ch);
+    }
+    pJ--;
+
+    if (is_palindrome(msg))
+        printf("Palindrome\n");
+    else
+        printf("Not a palindrome\n");
+
+    return 0;
+}
+
+bool is_palindrome(const char *message) {
+
+}
+
